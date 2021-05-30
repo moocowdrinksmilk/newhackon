7c3a7b524397e2b496595d217184df743a03bdbe
bingyuyap <bingyu.yap.21@gmail.com>
2021-05-30 10:50:54 +0800

    resolve conflict

--- times.py
+++ times.py
@@ -3,6 +3,7 @@ import config
 from typing import Dict
 import pprint
 import datetime
+from database import userDAO
 
 from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
 from telegram.ext import (
@@ -26,9 +27,16 @@ settime_markup = ReplyKeyboardMarkup(settime_keyboard, one_time_keyboard=True)
 def settime(update: Update, context: CallbackContext) -> int:
 
     """Start the conversation and ask user for input."""
-    user_id = update.message.from.id
+    user_id = update.message.chat.id
+    user = userDAO.get_user(user_id)
+
+    if user is None:
+        print('new user')
+        username = update.message.chat.username
+        userDAO.insert_user(user_id, username, None)
+
     update.message.reply_text(
-        "Hi! Welcome to TRYVE. What time would you like to TRYVE?",
+        f"Hi {user.username}! Welcome to TRYVE. What time would you like to TRYVE?",
         reply_markup=settime_markup,
     )
 
@@ -50,13 +58,18 @@ def duration_choice(update: Update, context: CallbackContext) -> int:
   
 def received_time(update: Update, context: CallbackContext) -> int:
     """Store info provided by user and ask for the next category."""
-    user_data = context.user_data
+    print(update)
+    user_id = update.message.chat.id
     time = update.message.text
 
     parse_time =  datetime.datetime.strptime(time, '%H:%M')
 
     print(parse_time)
 
+    userDAO.edit_set_time(user_id, parse_time)
+
     update.message.reply_text(
         f"Neat! We will suggest the activities at {parse_time.time()}")
+
+    
     return ConversationHandler.END
